20335107ce53970245034c7b93d0e5e8b950ae6a
Tripnaught <257220612+tripnaught@users.noreply.github.com>
2026-02-25 19:55:20 -0800

    lecture end 2/25/26

--- lectures/week5/linkedList.java
+++ lectures/week5/linkedList.java
@@ -1,70 +1,19 @@
 package week5;
 
 public class linkedList {
-    static Node<Integer> headNode = new Node<Integer>(-1); // this initial reference will never change; it's always the
-                                                           // head of the list
-    static Node<Integer> currentNode = headNode;
-
     public static void main(String[] args) {
-        Node<Integer> newNode = new Node<Integer>(8);
-        Node<Integer> newNode2 = new Node<Integer>(3);
-        Node<Integer> newNode3 = new Node<Integer>(8);
+        System.out.println("\u001b[36m");
 
-        newNode.next = newNode2;
-        newNode2.next = newNode3;
+        SinglyLinkedList<Integer> myList = new SinglyLinkedList<Integer>();
+        myList.append(2);
+        myList.append(3);
+        myList.append(5);
+        myList.append(7);
+        myList.append(11);
+        myList.print();
 
-    }
-
-    static void append(int newData) {
-        Node<Integer> newNode = new Node<Integer>(newData);
-
-        if (headNode == null) {
-            headNode = newNode;
-            return;
-        }
-
-        Node<Integer> curr = headNode; // it's not last yet!
-        while (curr.next != null) {
-            curr = curr.next;
-        }
-        // now we've reached the last node
-        curr.next = newNode;
-
-    }
-
-    static boolean search(int target) {
-        Node<Integer> curr = headNode;
-        while (curr != null) {
-            if (curr.data == target) {
-                return true;
-            }
-            curr = curr.next;
-        }
-        return false;
-    }
-
-    static boolean remove(int target) {
-        // case 1: linked list is empty
-        if (headNode == null) {
-            return false;
-        }
-
-        // case 2: need to remove the head of the linked list
-        if (headNode.data == target) {
-            headNode.next = headNode.next.next;
-            return true;
-        }
-
-        // case 3: need to remove anywhere else
-        Node<Integer> curr = headNode;
-        while (curr.next != null) {
-            if (curr.next.data == target) {
-                curr.next = curr.next.next; // bunnyhop
-                return true;
-            }
-            curr = curr.next; // advance
-        }
-        return false;
+        myList.search(7);
+        System.out.println(myList.search(10));
     }
 }
 
@@ -77,6 +26,7 @@ class SinglyLinkedList<T> {
         this.length = 0;
     }
 
+    // ================================= INSERTS =============================
     public void append(T data) {
         if (head == null) {
             head = new Node<T>(data);
@@ -103,38 +53,111 @@ class SinglyLinkedList<T> {
         }
     }
 
-    public void insertAfter() {
-        // ...
-    }
-
-    public void remove() {
-        // ...
-    }
-
-    public void search() {
-        // ...
-    }
-
-    public void print() {
+    public void insertAfter(T data, int index) {
         Node<T> curr = head;
-        while (curr.next != null) {
-            System.out.print(curr.data);
-            System.out.print(" -> ");
+        if (index == -1) {
+            prepend(data);
+            return;
+        } else if (index >= length || index < 0) { // restrict OOB
+            return;
+        }
+        Node<T> newNode = new Node<T>(data);
+        for (int i = 0; i < index; i++) {
             curr = curr.next;
         }
-        System.out.print("END");
+        newNode.next = curr.next;
+        curr.next = newNode;
+        length++;
     }
 
-    public void printReverse() {
+    public void insertAfter(T data, T afterThisData) {
+        insertAfter(data, search(afterThisData));
+    }
+
+    // ================================= REMOVES =============================
+    public Node<T> removeValue(T data) { // this returns the wrong thing
+        // base case 1: linked list is empty
+        if (isEmpty()) {
+            return null;
+        }
+
+        // base case 2: trying to remove the head
+        if (head.data.equals(data)) {
+            head = head.next;
+            return null;
+        }
+
+        // otherwise: loop through until data matches, then remove
+        Node<T> curr = head;
+        while (curr.next != null) {
+            if (curr.next.data.equals(data)) {
+                length--;
+                return curr.next = curr.next.next;
+            }
+            curr = curr.next;
+        }
+        return null;
+
+    }
+
+    public Node<T> removeIndex(int index) {
+        Node<T> curr = head;
+        Node<T> result = null;
+        if (index >= length || index < 0 || isEmpty()) { // restrict OOB
+            return null;
+        } else if (index == 0) {
+            result = head;
+            head = head.next;
+        } else {
+            for (int i = 0; i < index - 1; i++) {
+                curr = curr.next;
+            }
+            result = curr.next;
+            curr.next = (index < length - 1) ? curr.next.next : null;
+        }
+        length--;
+        return result;
+    }
+
+    // ================================= COMPLEX =============================
+    public int search(T target) {
         // ...
+        if (isEmpty() || head == null) {
+            return -1;
+        }
+        Node<T> curr = head;
+        int count = 0;
+        while (curr != null) {
+            if (curr.data.equals(target)) {
+                return count;
+            }
+            curr = curr.next;
+            count++;
+        }
+        return -1;
     }
 
     public void sort() {
         // ...
     }
 
+    // ================================= UTILS ================================
+    public void print() {
+        Node<T> curr = head;
+        while (curr != null) {
+            System.out.print(curr.data);
+            System.out.print(" -> ");
+            curr = curr.next;
+        }
+        System.out.println("END");
+    }
+
+    public void printReverse() {
+        // ...
+    }
+
     public boolean isEmpty() {
-        return (this.length == 0) ? true : false; 
+        return (this.length == 0 || this.head == null) ? true : false;
     }
 
     public int getLength() {
