e6fe734f4d2829d6e259260176d8a214639898e9
Tripnaught <257220612+tripnaught@users.noreply.github.com>
2026-03-11 19:53:00 -0700

    lecture end 3/11/26

--- homework/midterm/calcuator.java
+++ homework/midterm/calcuator.java
@@ -1,5 +1,108 @@
 package midterm;
 
+import java.util.Scanner;
+
 public class calcuator {
-	
+	public static Scanner scan = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		ExpressionTree exp = ExpressionTree.parse("123+456");
+		System.out.print(((Number) exp.root).value);
+	}
+}
+
+class ExpressionTree {
+	public final char[] VALID_SYMBOLS = {'+', '-', '*', '/', '^'};
+	public Node root;
+
+	private ExpressionTree(Node root) {
+		this.root = root;
+	}
+
+	public static ExpressionTree parse(String expression) {
+		Node root = null;
+		for (int i = 0; i < expression.length(); i++) {
+			char c = expression.charAt(i);
+			System.out.println(c);
+			if (Character.isDigit(c)) {
+				// the next node is going to be a number
+				double newValue = 0; 
+				while (i < expression.length() && Character.isDigit(expression.charAt(i))) {
+					newValue *= 10;
+					newValue += Character.getNumericValue(expression.charAt(i));
+					i++; // this advances the while-loop, and also
+					// prevents the for-loop from looking at digits twice.
+				}
+				i--;
+				root = new Number(newValue);
+			} else if (c == '+') { // symbols = Arrays.asList(strings).contains("Two")
+				Operation newOperation = new Operation(c);
+				newOperation.left = root;
+				root = newOperation;
+			}
+		}
+		return new ExpressionTree(root);
+	}
+}
+
+class Node {
+	public Node left;
+	public Node right;
+}
+
+class Number extends Node {
+	public double value;
+
+	public Number(double value) {
+		this.value = value;
+	}
+}
+
+class Operation extends Node {
+	char symbol;
+
+	public Operation(char symbol) {
+		this.symbol = symbol;
+	}
+
+	public double eval(Number a, Number b) {
+		switch (symbol) {
+			case '+':
+				return add(a, b);
+			case '-':
+				return subtract(a, b);
+			case '*':
+				return multiply(a, b);
+			case '/':
+				return divide(a, b);
+			case '^':
+				return exponent(a, b);
+			default:
+				System.out.printf("error: can't calculate %f %c %f\n", a.value, symbol, b.value);
+				return Double.NaN;
+		}
+	}
+
+	public double add(Number a, Number b) {
+		return a.value + b.value;
+	}
+
+	public double subtract(Number a, Number b) {
+		return a.value - b.value;
+	}
+
+	public double multiply(Number a, Number b) {
+		return a.value * b.value;
+	}
+
+	public double divide(Number a, Number b) {
+		if (b.value == 0) {
+			System.err.println("Error: ÷0");
+		}
+		return a.value / b.value;
+	}
+
+	public double exponent(Number a, Number b) {
+		return Math.pow(a.value, b.value);
+	}
 }
--- lectures/week7/hashtables.java
+++ lectures/week7/hashtables.java
@@ -2,13 +2,24 @@ package week7;
 
 public class hashtables {
 	public static void main(String[] args) {
-		System.out.println("guess we never tested this yet huh");
+		BasicHashTable abcs = new BasicHashTable(10);
+		abcs.put("a", "alpha");
+		abcs.put("b", "bravo");
+		abcs.put("c", "charlie");
+		abcs.put("d", "delta");
+		abcs.put("e", 2.718281828);
+		abcs.put("f", null);
+
+		System.out.println(abcs.lookup("e"));
+		System.out.println(abcs.lookup("f"));
+		System.out.println(abcs.lookup("b"));
 	}
 }
 
 class BasicHashTable {
 	private HashTableEntry[] table;
-	private int size;
+	private int size; // currently filled size (NOT total size i.e. capacity)
+	private double loadFactor; // size / table.length
 
 	public BasicHashTable(int capacity) {
 		table = new HashTableEntry[capacity];
@@ -24,6 +35,25 @@ class BasicHashTable {
 		return Math.abs(hashValue) % table.length;
 	}
 
+	private int hash2(String key) {
+		// lets be lazy for our 2nd hash function >:)
+		return Math.abs(key.hashCode()) % table.length;
+	}
+
+	// #region resize
+	// resize when loadFactor > .7
+	// create the new array
+	// rehash every item for our old array
+	public BasicHashTable resize(int newCapacity) {
+		BasicHashTable newHT = new BasicHashTable(newCapacity);
+		for (int i = 0; i < table.length; i++) {
+			if (table[i] != null && table[i] instanceof HashTableEntry) {
+				newHT.put(table[i].key, table[i].value);
+			}
+		}
+		return newHT;
+	}
+
 	// #region add
 	public void put(String key, Object value) {
 		// check for room to add
@@ -31,7 +61,7 @@ class BasicHashTable {
 			System.err.println("Error: Hash table full !!!1!1!!!!11!11");
 			return;
 		}
-		if (value.equals("DELETED")) {
+		if (value instanceof String && value.equals("DELETED")) {
 			System.err.println("Error: please don't enter the value 'DELETED'!!!");
 		}
 
@@ -67,7 +97,53 @@ class BasicHashTable {
 
 	}
 
-	// #region lookup
+	// # double probing
+	public void putDoubleProbing(String key, Object value) {
+		// check for room to add
+		if (size == table.length) {
+			System.err.println("Error: Hash table full !!!1!1!!!!11!11");
+			return;
+		}
+		if (value instanceof String && value.equals("DELETED")) {
+			System.err.println("Error: please don't enter the value 'DELETED'!!!");
+		}
+
+		// get the hash index based on key
+		int index1 = hash(key);
+		int index2 = hash2(key);
+		int startIndex1 = index1;
+		int startIndex2 = index2;
+		int i = 0;
+
+		// check if that index is occupied:
+		// if not yet occupied, insert and we're done
+		// else if already occupied, probe to next valid location and repeat
+		while (table[index1] != null && !table[index1].equals("DELETED")) {
+			// if we find key again, overwrite value at that position
+			if (table[index1].key.equals(key)) {
+				table[index1].value = value;
+				return;
+			}
+
+			// THIS IS WHERE THE MAGIC HAPPENS
+			index1 = (startIndex1 + i * index2) % table.length;
+			i++;
+
+			// once we get back to the original index, we found no free spots
+			// (this will not happen bc we checked at beginning)
+			if (index1 == startIndex1) {
+				System.err.println("No empty slot");
+				return;
+			}
+
+		}
+
+		table[index1] = new HashTableEntry(key, value);
+		size++;
+
+	}
+
+	// #region lookup aka get
 	public Object lookup(String key) {
 		// get hash index based on key
 		int index = hash(key);
@@ -80,7 +156,7 @@ class BasicHashTable {
 				return table[index].value;
 			}
 
-			index = (index + 1) % table.length;
+			index = (index + 1) % table.length; // use % to loop around
 
 		} while (table[index] != null && index != startIndex);
 
@@ -88,6 +164,58 @@ class BasicHashTable {
 		return null;
 
 	}
+
+	// #region get (prof's implementation of lookup)
+
+	// lookup
+	// get hash index based key
+	// loop: while key not found and not null -› probe to next location //if key
+	// found return value, if not, return null
+	public Object get(String key) {
+		int index = hash(key);
+		int startIndex = index;
+		while (table[index] != null) {
+			// handle key being found
+			if (!table[index].value.equals("DELETED") && table[index].key.equals(key)) {
+				return table[index].value;
+			}
+
+			// iterate
+			index = (index + 1) % table.length;
+
+			// exit if you've looked through everything
+			if (index == startIndex) {
+				break;
+			}
+		}
+		return null;
+	}
+
+	// #region remove
+	// we don't actually remove the HTEntry object, we just change its value to "DELETED"
+	// search for the item you want to remove. if you find it:
+	// change value to DELETED, decrement size, and break the loop.
+	public void remove(String key) {
+		int index = hash(key);
+		int startIndex = index;
+		while (table[index] != null) {
+			// handle key being found
+			if (!table[index].value.equals("DELETED") && table[index].key.equals(key)) {
+				table[index].value = "DELETED";
+				size--;
+				return;
+			}
+
+			// iterate
+			index = (index + 1) % table.length;
+
+			// exit if you've looked through everything
+			if (index == startIndex) {
+				break;
+			}
+		}
+	}
+
 }
 
 class HashTableEntry {
